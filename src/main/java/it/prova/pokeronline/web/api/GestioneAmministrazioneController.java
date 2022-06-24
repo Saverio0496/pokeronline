b5dfc8e052ded6907cbae91cd10917fcf494f6e4
Saverio Carelli <save.carelli@gmail.com>
2022-06-25 01:23:56 +0200

    Implementazione findById di Utente in GestioneAmministrazione

--- src/main/java/it/prova/pokeronline/web/api/GestioneAmministrazioneController.java
+++ src/main/java/it/prova/pokeronline/web/api/GestioneAmministrazioneController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,10 @@ import it.prova.pokeronline.dto.UtenteDTO;
 import it.prova.pokeronline.model.Utente;
 import it.prova.pokeronline.service.UtenteService;
 import it.prova.pokeronline.web.api.exception.IdNotNullForInsertException;
+import it.prova.pokeronline.web.api.exception.UtenteNotFoundException;
+import it.prova.raccoltafilmspringrest.dto.FilmDTO;
+import it.prova.raccoltafilmspringrest.model.Film;
+import it.prova.raccoltafilmspringrest.web.api.exception.FilmNotFoundException;
 
 @RestController
 @RequestMapping("/api/gestioneAmministrazione")
@@ -39,6 +44,16 @@ public class GestioneAmministrazioneController {
 
 		Utente utenteInserito = utenteService.inserisciNuovo(utenteInput.buildUtenteModel(true));
 		return UtenteDTO.buildUtenteDTOFromModel(utenteInserito);
+	}
+
+	@GetMapping("/{id}")
+	public UtenteDTO findById(@PathVariable(value = "id", required = true) long id) {
+		Utente utente = utenteService.caricaSingoloUtenteConRuoli(id);
+
+		if (utente == null)
+			throw new UtenteNotFoundException("Utente not found con id: " + id);
+
+		return UtenteDTO.buildUtenteDTOFromModel(utente);
 	}
 
 }
