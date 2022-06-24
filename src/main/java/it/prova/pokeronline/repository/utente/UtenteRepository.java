64b30fe2621a9649d8525d76b69f166981ea208d
Saverio Carelli <save.carelli@gmail.com>
2022-06-24 16:29:14 +0200

    Implementazione security di base

---
+++ src/main/java/it/prova/pokeronline/repository/utente/UtenteRepository.java
@@ -0,0 +1,14 @@
+package it.prova.pokeronline.repository.utente;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.pokeronline.model.Utente;
+
+public interface UtenteRepository extends CrudRepository<Utente, Long> {
+
+	@EntityGraph(attributePaths = { "ruoli" })
+	Optional<Utente> findByUsername(String username);
+}
